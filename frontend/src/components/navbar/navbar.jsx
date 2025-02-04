a1da187351f00e092071bf2fa69766f157cdae80
AmeenShakkib <ameenshakkib@gmail.com>
2025-02-04 19:01:57 +0530

    again changes

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -7,13 +7,19 @@ const Navbar = () => {
         <div className="topnav">
             <ul>
                 <li>
+                    <div className="link"> 
                     <NavLink to='/' className={({isActive})=>(isActive ? "active" : "")}>Home</NavLink>
+                    </div> 
                 </li>
                 <li>
-                    <NavLink to='/addBirthday' className={({isActive})=>(isActive ? "active" : "")}>Add Birthday</NavLink>
+                    <div className="link">
+                    <NavLink to='/addBirthday' className={({isActive})=>(isActive ? "active" : "")}>Add Bday</NavLink>
+                    </div>
                 </li>
                 <li>
-                    <NavLink to="/CelebritysBirthday" className={({isActive})=>(isActive ? "active" : "")}>Get a Celebrity's Birthday</NavLink>
+                    <div className="link">
+                    <NavLink to="/CelebritysBirthday" className={({isActive})=>(isActive ? "active" : "")}>Celebrity's Bday</NavLink>
+                    </div>
                 </li>
             </ul>
         </div>
